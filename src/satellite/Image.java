33473953ab98140599eec1b36a284bdf5ff8efe4
Gabriel <gabrielws31@gmail.com>
2019-09-07 16:05:14 -0300

    criada claasse de imagem

--- src/satellite/Image.java
+++ src/satellite/Image.java
@@ -1,13 +1,6 @@
 package satellite;
 
 public class Image {
-    private int teste;
 
-    public int getTeste() {
-        return teste;
-    }
 
-    public void setTeste(int teste) {
-        this.teste = teste;
-    }
 }
