1a5974a340be4149d7764af9f446cf1849039e81
Gabriel <gabrielws31@gmail.com>
2019-09-07 15:10:24 -0300

    initializing repository

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import satellite.Image;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/satellite/Image.java
@@ -0,0 +1,13 @@
+package satellite;
+
+public class Image {
+    private int teste;
+
+    public int getTeste() {
+        return teste;
+    }
+
+    public void setTeste(int teste) {
+        this.teste = teste;
+    }
+}
